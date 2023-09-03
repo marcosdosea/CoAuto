f70b1b4540ff96ca0556f3e28bcb7ab871497459
jordanaquino <contato.jordanaquino@gmail.com>
2023-09-03 17:30:36 -0300

    criação do projeto web

---
+++ Codigo/CoAutoWeb/Core/Banco.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core;
+
+public partial class Banco
+{
+    public uint Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public virtual ICollection<Pessoa> Pessoas { get; set; } = new List<Pessoa>();
+}
---
+++ Codigo/CoAutoWeb/Core/Devolucao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core;
+
+public partial class Devolucao
+{
+    public uint Id { get; set; }
+
+    public byte[]? Foto1 { get; set; }
+
+    public byte[]? Foto2 { get; set; }
+
+    public byte[]? Foto3 { get; set; }
+
+    public byte[]? Foto4 { get; set; }
+
+    public byte[]? Foto5 { get; set; }
+
+    public DateTime DataHora { get; set; }
+}
