33019ccfc645d770d980633803a816707380b916
jordanaquino <contato.jordanaquino@gmail.com>
2023-09-04 10:05:03 -0300

    Implementação do caso de uso manter veículo

---
+++ Codigo/CoAutoWeb/CoAutoWeb/Mappers/VeiculoProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CoAutoWeb.Models;
+using Core;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Core;
+
+namespace CoAutoWeb.Mappers
+{
+    public class VeiculoProfile : Profile
+    {
+        public VeiculoProfile()
+        {
+            CreateMap<VeiculoViewModel, Veiculo>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/CoAutoWeb/Core/DTO/VeiculoDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class VeiculoDTO
+    {
+        public string Tipo { get; set; } = null!;
+
+        public string Ano { get; set; } = null!;
+
+        public int? Cilindradas { get; set; }
+
+        public float Valor { get; set; }
+
+        public string Estado { get; set; } = null!;
+
+        public string Cidade { get; set; } = null!;
+
+        public string? Portas { get; set; }
+
+        public int NomeModelo { get; set; }
+
+        public string Combustivel { get; set; } = null!;
+
+        public string Cambio { get; set; } = null!;
+
+        public string? Passageiro { get; set; }
+
+        public string? Carroceria { get; set; }
+
+        public uint NomePessoa { get; set; }
+
+        public virtual ICollection<Disponibilidade> Disponibilidades { get; set; } = new List<Disponibilidade>();
+
+        public virtual Modelo NomeModeloNavigation { get; set; } = null!;
+
+        public virtual Pessoa NomePessoaNavigation { get; set; } = null!;
+    }
+}
