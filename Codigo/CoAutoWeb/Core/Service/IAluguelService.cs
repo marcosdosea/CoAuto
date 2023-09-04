046789f023d82e9f2c369ab402724b95ddef2e18
NagibeJR <junior.wanus@gmail.com>
2023-09-04 14:41:49 -0300

    Implementado classe de serviço referentes ao caso de uso Alugar Veiculo. AluguelDTO.cs, AluguelService.cs, IAluguelService.cs

---
+++ Codigo/CoAutoWeb/Core/Service/IAluguelService.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Service
+{
+    public interface IAluguelService
+    {
+        int Create(Aluguel aluguel);
+        Aluguel Delete(int idAluguel);
+        void Edit(Aluguel aluguel);
+        Aluguel Get(int idAluguel);
+        IEnumerable<Aluguel> GetAll();
+    }
+}
---
+++ Codigo/CoAutoWeb/Service/AluguelService.cs
@@ -0,0 +1,75 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service
+{
+    public class AluguelService : IAluguelService
+    {
+        private readonly CoAutoContext context;
+
+        public AluguelService(CoAutoContext context)
+        {
+            this.context = context;
+        }
+
+        /// <summary>
+        /// Cria um novo aluguel
+        /// </summary>
+        /// <param name="aluguel">dados do aluguel</param>
+        /// <returns>id do Aluguel</returns>
+
+        public int Create(Aluguel aluguel)
+        {
+            context.Add(aluguel);
+            context.SaveChanges();
+            return aluguel.Id;
+        }
+
+        public void delete(Aluguel aluguel)
+        {
+            context.Remove(aluguel);
+            context.SaveChanges();
+        }
+
+        public Aluguel Get(int idAluguel)
+        {
+            return context.Aluguels.Find(idAluguel);
+
+        }
+
+        /// <summary>
+        /// Busca todos os Aluguels na base de dados
+        /// </summary>
+        /// <returns>lista de alugueis</returns> 
+
+        public IEnumerable<Aluguel> GetAll()
+        {
+            return context.Aluguels.AsNoTracking();
+        }
+
+        /// <summary>
+        /// Edita os dados do Aluguel na base de dados
+        /// </summary>
+        /// <param name="aluguel"></param>
+
+        public void Edit(Aluguel aluguel)
+        {
+            context.Update(aluguel);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Remove os dados do Aluguel na base de dados
+        /// </summary>
+        /// <param name="idAluguel"></param>
+
+        public Aluguel Delete(int idAluguel)
+        {
+            var _aluguel = context.Aluguels.Find(idAluguel);
+            context.Remove(_aluguel);
+            context.SaveChanges();
+            return context.Aluguels.Find(idAluguel);
+        }
+    }
+}
