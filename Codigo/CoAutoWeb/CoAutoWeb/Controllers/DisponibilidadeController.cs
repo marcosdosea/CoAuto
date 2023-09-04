b71b7ffb13a483e106cd72af81599e6cae1100bd
Luis Gabriel <logxpbr@gmail.com>
2023-09-04 18:11:22 -0300

    começo da controller

--- Codigo/CoAutoWeb/CoAutoWeb/Controllers/DisponibilidadeController.cs
+++ Codigo/CoAutoWeb/CoAutoWeb/Controllers/DisponibilidadeController.cs
@@ -1,20 +1,44 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using CoAutoWeb.Models;
+using Core.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service;
 
 namespace CoAutoWeb.Controllers
 {
     public class DisponibilidadeController : Controller
     {
+
+        private readonly IDisponibilidadeService _disponibilidadeService;
+        private readonly IMapper _mapper;
+
+
+
+        public DisponibilidadeController(IDisponibilidadeService disponibilidadeService, IMapper mapper)
+        {
+            _disponibilidadeService = disponibilidadeService;
+            _mapper = mapper;
+        }
+
+
+
+
+
         // GET: DisponibilidadeController
         public ActionResult Index()
         {
-            return View();
+            var listaDisponibilidades = _disponibilidadeService.GetAll();
+            var listaDisponibilidadeModel = _mapper.Map<List<DisponibilidadeModel>>(listaDisponibilidades);
+            return View(listaDisponibilidadeModel);
         }
 
         // GET: DisponibilidadeController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var disponibilidade = _disponibilidadeService.GetAll();
+            var disponibilidadeViewModel = _mapper.Map<List<disponibilidadeViewModel>>(disponibilidade);
+            return View(disponibilidadeViewModel);
         }
 
         // GET: DisponibilidadeController/Create
