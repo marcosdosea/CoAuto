3c209329647655026bcb8c9f8b645035d1e60e90
Luis-clr <logxpbr@gmail.com>
2023-09-05 00:29:31 -0300

    Resolvendo o problema de pacotes do caso de uso disponibilizar veiculo

---
+++ Codigo/CoAuto/CoAutoWeb/Mappers/DisponibilidadeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CoAutoWeb.Models;
+using Core;
+
+
+namespace CoAutoWeb.Mappers
+{
+    public class DisponibilidadeProfile : Profile
+    {
+        public DisponibilidadeProfile()
+        {
+            CreateMap<DisponibilidadeModel, Disponibilidade>().ReverseMap();   
+        }
+    }
+}
