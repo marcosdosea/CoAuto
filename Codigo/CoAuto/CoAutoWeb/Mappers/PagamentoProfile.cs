aca51a331145043568c5fe10a2a570036ccbff9b
muzanpvp <mkibutsuji4@gmail.com>
2023-09-09 11:17:19 -0300

    Implementado Model, Service, Controller e view do caso de uso Realizar Pagamento. Closed #125. Closed #

---
+++ Codigo/CoAuto/CoAutoWeb/Mappers/PagamentoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CoAutoWeb.Models;
+using Core;
+
+namespace CoAutoWeb.Mappers
+{
+    public class PagamentoProfile : Profile
+    {
+        public PagamentoProfile()
+        {
+            CreateMap<PagamentoViewModel,Pagamento>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/CoAuto/Core/Service/IPagamentoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Core.Service
+{
+    public interface IPagamentoService
+    {
+        int Create(Pagamento pagamento);
+        void Edit(Pagamento pagamento);
+        void Delete(int idPagamento);
+        Pagamento Get(int id);
+
+        IEnumerable<Pagamento> GetAll(); 
+    }
+}
