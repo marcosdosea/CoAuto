aff10ec538e603b4ee5cae9650b87bb465ff2c6b
Luis-clr <logxpbr@gmail.com>
2023-09-25 11:29:22 -0300

    criação dos teste

---
+++ Codigo/CoAuto/CoAutoWebTests/Controllers/DisponibilidadeControllerTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoAutoWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Service;
+using AutoMapper;
+
+namespace CoAutoWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class DisponibilidadeControllerTests
+    {
+        private static DisponibilidadeController controller;
+        private IDisponibilidadeService @object;
+        private IMapper mapper;
+
+
+        [TestMethod()]
+        public void DisponibilidadeControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
