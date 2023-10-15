18396864db8f35d192abc463c35df55ccc24f220
NagibeJR <junior.wanus@gmail.com>
2023-10-15 15:55:15 -0300

    Implementado da Autentificação e Autorização .Closed #231

--- Codigo/CoAuto/CoAutoWeb/Program.cs
+++ Codigo/CoAuto/CoAutoWeb/Program.cs
@@ -2,6 +2,9 @@ using Core;
 using Core.Service;
 using Microsoft.EntityFrameworkCore;
 using Service;
+using Microsoft.AspNetCore.Identity;
+using CoAutoWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace CoAutoWeb;
 
@@ -13,9 +16,6 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
-
-        builder.Services.AddDbContext<CoAutoContext>(
-            options => options.UseMySQL(builder.Configuration.GetConnectionString("CoAutoDatabase")));
 
         builder.Services.AddTransient<IVeiculoService, VeiculoService>();
         builder.Services.AddTransient<IDisponibilidadeService, DisponibilidadeService>();
@@ -30,6 +30,51 @@ public class Program
 
         builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+        builder.Services.AddDbContext<CoAutoContext>(
+            options => options.UseMySQL(builder.Configuration.GetConnectionString("CoAutoDatabase")));
+
+        builder.Services.AddDbContext<IdentityContext>(
+            options => options.UseMySQL(builder.Configuration.GetConnectionString("CoAutoDatabase")));
+
+
+        builder.Services.AddDefaultIdentity<UsuarioIdentity>(options =>
+        {
+            // SignIn settings
+            options.SignIn.RequireConfirmedAccount = false;
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // Password settings
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+
+            // Default User settings.
+            options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = false;
+
+            // Default Lockout settings
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+        }).AddRoles<IdentityRole>()
+          .AddEntityFrameworkStores<IdentityContext>();
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            options.Cookie.Name = "YourAppCookieName";
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+            options.LoginPath = "/Identity/Account/Login";
+            // ReturnUrlParameter requires 
+            options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+            options.SlidingExpiration = true;
+        });
+
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
@@ -39,18 +84,15 @@ public class Program
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
-        else
-        {
-            SeedingService seed = new SeedingService(app.Services.CreateScope().ServiceProvider.GetRequiredService<CoAutoContext>());
-            seed.Seed();
-        }
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
         app.UseRouting();
+        app.UseAuthentication(); ;
 
         app.UseAuthorization();
+        app.MapRazorPages();
 
         app.MapControllerRoute(
             name: "default",
@@ -59,3 +101,4 @@ public class Program
         app.Run();
     }
 }
+
